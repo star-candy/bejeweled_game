b309a35ef29555eee5ec56c53f32438713ef38c4
star-candy <el0710@naver.com>
2024-05-12 16:49:12 +0900

    초기 커밋.

---
+++ main_text.cpp
@@ -0,0 +1,25 @@
+#include "Puzzle.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::vector<std::string> predefined_puzzles = {
+	"!#!&%*&@&!@&!!@#!@$$**%!&!&&!##&#*@$&@$%%$$*&*@$##$#@$%@#$&#%$@#",
+	"#!%%@%!&@*%!&@&!#*$$%%%&#*$#@$@!$%$@%@&!%$&%&@*%*$&&*&#!$$&*$#*!",
+	"*@&*@#%%&%%&!$!*%#%*!*##*$$###*$$!#&&@*$$@#&#$&$$#!!!**@##@@@!!!",
+	"$#@!%@$#$&$&!!*@@!$$@$!&*@**&$&@$!#*@&*@&###!@@%&@&!%&&%##$#@@&$",
+};
+
+/*
+* Declare and define as many functions as necessary
+*/
+
+int main()
+{
+	/*
+	* Implement text-based UI for playing Bejeweled game as required
+	*/
+
+	return 0;
+}
