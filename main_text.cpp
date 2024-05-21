1d1a79b492553e5006d91cc0dbe4c05d94d429fc
star-candy <el0710@naver.com>
2024-05-21 09:52:15 +0900

    sync

--- main_text.cpp
+++ main_text.cpp
@@ -26,10 +26,8 @@ int main()
 			updatecount = text.swapScreen();
 		}
 		repeatcount = text.initialScreen(predefined_puzzles);
-		//update a에서 update 기능 종료 시 다음 update에서 바로 b 기능으로 시작되는 문제
-		//static 카운터를 private 변수로 만들고 while문 재시작 시 true 선언
-	} while (repeatcount != 3 && repeatcount > 0 && repeatcount < 3);
 
+	} while (repeatcount != 3 && repeatcount > 0 && repeatcount < 3);
 
 	return 0;
 }
